0b82cae854880311fddd7412f5b66587ebc09941
Husayn Mubarak Adelokun <husaynmubarak0@gmail.com>
2023-07-24 10:07:40 +0100

    init

---
+++ cal.js
@@ -0,0 +1,7 @@
+const type = new Typed('#changeText', {
+    strings: ['Developed by:', 'DevHusayn', 'A React programmer.'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
